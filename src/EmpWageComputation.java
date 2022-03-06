6b366ff93c862b0c6ef04d7c1d9218e9d8320f70
yashdchintawar <yashchintawar31@gmail.com>
2022-03-06 15:57:44 +0530

    Uc-2 checking employee is present or absent

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,6 +1,16 @@
+import java.util.Random;
+
 public class EmpWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation");
+        int randomCheck = (int)Math.floor(Math.random()*2);
+
+        if (randomCheck == 0){
+            System.out.println("Employee Is Present");
+        }
+        else{
+            System.out.println("Employee Is Absent");
+        }
     }
 
 }
