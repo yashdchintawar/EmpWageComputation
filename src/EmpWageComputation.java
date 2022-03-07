b788df00fdd51bd02b1b5c780b2866dc4fc067b3
yashdchintawar <yashchintawar31@gmail.com>
2022-03-07 10:04:57 +0530

    Emplyee calculation till 20 days or 100 hours

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,40 +1,47 @@
 import java.util.Random;
 
-public class EmpWageComputation {
+public class EmpWageComputation<empCalculationMonthly> {
     static int Wage_Per_Hour = 20;
     static int Full_Day_Hour = 8;
     static int Half_Day_Hour = 4;
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation");
-        int empCalculationMonthly = 0;
 
-        for (int i = 0 ; i < 20 ; i++) {
+        System.out.println("Welcome To Employee Wage Computation");
+
+        int empCalculationMonthly = 0;
+        int empDaysCount = 1;
+        int empHoursCount = 0;
+
+        while(empDaysCount<20 && empHoursCount < 100 ) {
 
             int presntOrAbsent = (int) Math.floor(Math.random() * 2);
             int isPartOrFullTime = (int) Math.floor(Math.random() * 2);
 
-            switch (presntOrAbsent) {
-                case 0:
-                    System.out.println("Employee is Present");
-                    switch (isPartOrFullTime) {
-                        case 0:
-                            int Emp_Full_Day_Wage = Wage_Per_Hour * Full_Day_Hour;
-                            System.out.println("Employee Full Day Wage Is: " + Emp_Full_Day_Wage);
-                            empCalculationMonthly += Emp_Full_Day_Wage;
-                            break;
-
-                        default:
-                            int Emp_Half_Day_Wage = Wage_Per_Hour * Half_Day_Hour;
-                            System.out.println("Employee Half Day Wage Is: " + Emp_Half_Day_Wage);
-                            empCalculationMonthly += Emp_Half_Day_Wage;
+            if (presntOrAbsent == 0) {
+                System.out.println("Employee is Present");
+                if (empHoursCount < 92) {
+                    if (isPartOrFullTime == 0) {
+                        int Emp_Full_Day_Wage = Wage_Per_Hour * Full_Day_Hour;
+                        System.out.println("Employee Full Day Wage Is: " + Emp_Full_Day_Wage);
+                        empCalculationMonthly += Emp_Full_Day_Wage;
+                        empHoursCount += 8;
                     }
-                    break;
-                default:
+                }
+                else {
+                    int Emp_Half_Day_Wage = Wage_Per_Hour * Half_Day_Hour;
+                    System.out.println("Employee Half Day Wage Is: " + Emp_Half_Day_Wage);
+                    empCalculationMonthly += Emp_Half_Day_Wage;
+                    empHoursCount += 4;
+                }
+
+                empDaysCount++;
+            }
+                else{
                     System.out.println("Employee is Absent");
             }
-        }
+            }
         System.out.println("Total Monthly Employee Wage is: " + empCalculationMonthly);
-
+        System.out.println("Total Days Employee Wage is: " + empDaysCount);
+        System.out.println("Total Hours Employee Wage is: " + empHoursCount);
+        }
     }
-
-}
