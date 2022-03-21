eb09948e4f97ee236ce2d906245e03b49dc736eb
yashdchintawar <yashchintawar31@gmail.com>
2022-03-21 11:28:50 +0530

    Now We can save the employee Wage

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -5,31 +5,42 @@ public class EmpWageComputation{
     final static int FULL_DAY = 8;
     final static int HALF_DAY = 4;
 
+    private final String empCompanyName;
+    private final int empWagePerHour;
+    private final int empMaxNumberOfHours;
+    private final int empMaxNumberOfDays;
+    private int empCalculationMonthly;
+    private int empHoursCount;
+    private int empDaysCount;
 
-    public static void empwagecalculation(String empCompanyName, int empWagePerHour, int empMaxNumberOfHours, int empMaxNumberOfDays) {
+    public EmpWageComputation(String empCompanyName, int empWagePerHour, int empMaxNumberOfHours, int empMaxNumberOfDays){
+        this.empCompanyName = empCompanyName;
+        this.empWagePerHour = empWagePerHour;
+        this.empMaxNumberOfHours = empMaxNumberOfHours;
+        this.empMaxNumberOfDays = empMaxNumberOfDays;
+
+    }
+
+
+    public void empwagecalculation() {
 
         System.out.println("Welcome To Employee Wage Computation"); //welcome message
 
-        int empCalculationMonthly = 0;
-        int empDaysCount = 1;
-        int empHoursCount = 0;
+        while (empDaysCount < empMaxNumberOfDays && empHoursCount < empMaxNumberOfHours) {
 
-        while(empDaysCount<empMaxNumberOfDays && empHoursCount < empMaxNumberOfHours ) {
-
-            int presntOrAbsent = (int) Math.floor(Math.random() * 2);
+            int presentOrAbsent = (int) Math.floor(Math.random() * 2);
             int isPartOrFullTime = (int) Math.floor(Math.random() * 2);
 
-            if (presntOrAbsent == 0) {
+            if (presentOrAbsent == 0) {
 //                System.out.println("Employee is Present");
-                if (empHoursCount < 92) {
+                if (empHoursCount < (empMaxNumberOfHours - 8)) {
                     if (isPartOrFullTime == 0) {
                         int Emp_Full_Day_Wage = empWagePerHour * FULL_DAY;
 //                        System.out.println("Employee Full Day Wage Is: " + Emp_Full_Day_Wage);
                         empCalculationMonthly += Emp_Full_Day_Wage;
                         empHoursCount += 8;
                     }
-                }
-                else {
+                } else {
                     int Emp_Half_Day_Wage = empWagePerHour * HALF_DAY;
 //                    System.out.println("Employee Half Day Wage Is: " + Emp_Half_Day_Wage);
                     empCalculationMonthly += Emp_Half_Day_Wage;
@@ -37,37 +48,32 @@ public class EmpWageComputation{
                 }
 
                 empDaysCount++;
-            }
-                else{
+            } else {
 //                    System.out.println("Employee is Absent");
             }
-            }
-        System.out.println("Employee Company Is: " + empCompanyName);
-        System.out.println("Total Monthly Employee Wage is: " + empCalculationMonthly);
-        System.out.println("Total Days Employee Wage is: " + empDaysCount);
-        System.out.println("Total Hours Employee Wage is: " + empHoursCount);
+        }
+    }
+        @Override
+        public String toString () {
+            return '\n' + "EmpWageComputation Is " + '\n' +
+                    "employee Company Name Is = " + " " + empCompanyName + '\n' +
+                    "employee Wage Per Hour Is = " + " " + empWagePerHour + '\n' +
+                    "employee Maximum Number Of Hours Is = " + " " + empMaxNumberOfHours + '\n' +
+                    "employee Maximum Number Of Days Is = " + " " + empMaxNumberOfDays + '\n' +
+                    "employee Total Worked Hours In Month Is = " + " " + empHoursCount + '\n' +
+                    "employee Total Worked Days In Month Is = " + " " + empDaysCount + '\n' +
+                    "employee Total Wage monthly Calculation Is = " + " " + empCalculationMonthly + '\n';
         }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        EmpWageComputation company1 = new EmpWageComputation("dmart", 35,120, 25);
+        EmpWageComputation company2 = new EmpWageComputation("Infy", 30,110, 22);
 
-        System.out.println("How Many Number Of Companies Do You Want To Calculate");
-        int companyNumber = in.nextInt();
+        company1.empwagecalculation();
+        System.out.println(company1);
 
-        for (int i = 0; i<companyNumber ; i++) {
-            System.out.println("Enter Company Name");
-            String companyName = in.next();
+        company2.empwagecalculation();
+        System.out.println(company2);
 
-            System.out.println("Enter Wage Per Hour");
-            int wagePerHour = in.nextInt();
-
-            System.out.println("Enter Maximum Number Of Hours");
-            int maxNumberOfHours = in.nextInt();
-
-            System.out.println("Enter Maximum Number Of Days");
-            int maxNumberOfDays = in.nextInt();
-
-            empwagecalculation(companyName, wagePerHour, maxNumberOfHours, maxNumberOfDays);
         }
-    }
     }
